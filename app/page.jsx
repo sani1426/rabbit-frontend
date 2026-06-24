c7886407a172ec63c4dbba676acc3ea9f96fbdd0
saman <sam.karimi727@gmail.com>
2026-06-25 01:00:03 +0330

    update 3 files, delete 7 files and create 5 files

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>click here</Button>
+    </div>
+  )
+}
+
+export default HomePage
