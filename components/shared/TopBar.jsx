4fcff7707843878de138b7c418620843bfd4fa8e
saman <sam.karimi727@gmail.com>
2026-06-25 02:54:52 +0330

    build(deps): update TopBar.jsx, package-lock.json and package.json

--- components/shared/TopBar.jsx
+++ components/shared/TopBar.jsx
@@ -1,24 +1,31 @@
-import Image from "next/image"
-import Link from "next/link"
-
+import { TbBrandMeta } from 'react-icons/tb'
+import { IoLogoInstagram } from 'react-icons/io'
+import { RiTwitterXLine } from 'react-icons/ri'
 
 const TopBar = () => {
   return (
-    <div className="bg-[#fd802e] text-white">
-        <div className="container mx-auto">
-                <div className="flex items-center space-x-4">
-                        <Link className="hover:text-gray-300" href="/">
-                                <Image src="@/public/icons/facebook.svg" alt="facebook-icon" className="w-5 h-5" />
-                        </Link>
-                        <Link className="hover:text-gray-300" href="/">
-                                <Image src="@/public/icons/instagram.svg" alt="facebook-icon" className="w-5 h-5" />
-                        </Link>
-                        <Link className="hover:text-gray-300" href="/">
-                                <Image src="@/public/icons/twitter.svg" alt="facebook-icon" className="w-5 h-5" />
-                        </Link>
-                </div>
-                <div className=""></div>
+    <div className='bg-[#5b0e14] text-white'>
+      <div className='container mx-auto flex justify-between items-center py-3 px-4'>
+        <div className='hidden md:flex items-center space-x-4'>
+          <a className='hover:text-gray-300' href='/'>
+            <TbBrandMeta className='h-5 w-5' />
+          </a>
+          <a className='hover:text-gray-300' href='/'>
+            <IoLogoInstagram className='h-5 w-5' />
+          </a>
+          <a className='hover:text-gray-300' href='/'>
+            <RiTwitterXLine className='h-5 w-5' />
+          </a>
         </div>
+        <div className='text-sm text-center flex-gow'>
+          <span>ارسال به تمام مناطق کشور با سریع ترین زمان ممکن</span>
+        </div>
+        <div className='text-sm hidden md:block'>
+          <a href='tel:+989914318174' className='hover:text-gray-300'>
+            +98 (991) 431 8174
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
